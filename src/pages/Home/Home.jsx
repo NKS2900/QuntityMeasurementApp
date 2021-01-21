5a4a8a656657fbd7fba457b71b3c02f9e3d62250
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-01-21 11:16:07 +0530

    [NIJAM] Add:Quntity Measurement UI.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,97 @@
+import React,{ Component} from 'react';
+import {AppBar,Card, MenuItem,TextField} from '@material-ui/core'
+import length from '../../assets/length.png';
+import temperature from '../../assets/temp.png';
+import volumn from '../../assets/volume.png';
+import './home.css';
+
+class Home extends Component{
+    constructor(){
+        super();
+        this.state={len:''}
+    }
+    handleChange=(event)=>{
+        let len=event.target.value;
+        console.log("length: ",len);
+        this.setState({len:event.target.value})
+    }
+    render(){
+        return(
+            <div className="homeContainer">
+            <AppBar id="welcomeBar">
+                <h1>Welcome to Quantity Measurement </h1>
+            </AppBar>
+            <div id="chooseType">
+                        <lable>CHOOSE TYPE</lable>
+                    </div> 
+            <div className="cardContainer">
+                        <Card id="length">
+                        <div id="imgLen">
+                            <img src={length} alt="length"/>
+                        </div>
+                        <div>
+                           <label id="lengthLabel"> Length</label>
+                        </div>
+                        </Card>
+                        <Card id="temperature">
+                        <div id="imgLen" >
+                            <img src={temperature} alt="temperature"/>
+                        </div>
+                        <div>
+                            <label id="temperatureLable"> Temperature</label>
+                        </div>
+                        </Card>
+                        <Card id="volumn">
+                        <div id="imgLen">
+                            <img src={volumn} alt="volumn"/>
+                        </div>
+                        <div>
+                            <label id="volumeLabel">Volume</label>
+                        </div>
+                        </Card>
+                    </div>
+            <div className="converterContainer">
+                <div>
+                <div id="labelCol"><label id="select-label">FROM</label></div>
+                <div id="convertFromContainer">
+                <div><TextField className="TextField" type="number" variant="outlined" size="small" ></TextField></div>
+                  
+                <select className="selectID" value={this.state.len}onChange={this.handleChange}>
+                <MenuItem value=""><em>None</em></MenuItem>
+                            <MenuItem value={20}>Kilometre</MenuItem>
+                            <MenuItem value={20}>Metres</MenuItem>
+                            <MenuItem value={10}>Centimetres</MenuItem>
+                            <MenuItem value={30}>Milimetre</MenuItem>
+                            <MenuItem value={10}>Micrometre</MenuItem>
+                            <MenuItem value={40}>Mile</MenuItem>
+                            <MenuItem value={50}>Foot</MenuItem>
+                            <MenuItem value={60}>Inch</MenuItem> 
+                </select>  
+                </div>
+                </div>
+                
+                <div id="labelCol2">
+                <label id="select-label">TO</label>
+                <div id="convertToContainer">
+                <div><TextField className="TextField" type="number" variant="outlined" size="small" ></TextField></div>
+                <select className="selectID">
+                        <MenuItem value=""><em>None</em></MenuItem>
+                        <MenuItem value={20}>Kilometre</MenuItem>
+                        <MenuItem value={20}>Metres</MenuItem>
+                        <MenuItem value={10}>Centimetres</MenuItem>
+                        <MenuItem value={30}>Milimetre</MenuItem>
+                        <MenuItem value={10}>Micrometre</MenuItem>
+                        <MenuItem value={40}>Mile</MenuItem>
+                        <MenuItem value={50}>Foot</MenuItem>
+                        <MenuItem value={60}>Inch</MenuItem>
+                </select>
+                </div>
+                </div>
+            </div>                         
+            </div>
+        )
+    }
+
+}
+
+export default Home;
